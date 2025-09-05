a872917f151be833d1008c6917bb01e141933e96
kino1022 <yuta_fujita@outlook.com>
2025-09-06 00:25:29 +0900

    Update CorrectionTypePropertyProvider.cs

--- Correction/Definition/Type/CorrectionTypePropertyProvider.cs
+++ Correction/Definition/Type/CorrectionTypePropertyProvider.cs
@@ -2,10 +2,11 @@ using GeneralModule.Correction.Definition.Type.Interface;
 using Sirenix.Serialization;
 using System;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 
 namespace GeneralModule.Correction.Definition.Type {
     [Serializable]
-    public partial class CorrectionTypePropertyProvider : ICorrectionTypePropertyProvider {
+    public class CorrectionTypePropertyProvider : SerializedScriptableObject, ICorrectionTypePropertyProvider {
 
         [OdinSerialize]
         protected Dictionary<CorrectionType, ICorrectionTypeProperty> m_properties = new();
